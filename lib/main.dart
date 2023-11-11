9e4df4f33c31dad5a32b5434897d0758a7c928de
durga <21p31a04N8@acet.ac.in>
2023-11-11 14:09:07 +0530

    Technologies added1

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,8 @@ import 'package:myapplication/Home.dart';
 import 'package:sizer/sizer.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
